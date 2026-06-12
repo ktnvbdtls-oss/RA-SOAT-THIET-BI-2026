b80549a4144bbfbbcba6afdfdba45bafc965d7ac
ktnvbdtls-oss <ktnvbdtls@gmail.com>
2026-06-12 09:42:29 +0700

    Update github-config.js

--- static/github-config.js
+++ static/github-config.js
@@ -3,11 +3,11 @@
  * Token được thiết lập qua giao diện ứng dụng và lưu trong localStorage của trình duyệt.
  *
  * URL GitHub Pages sau khi deploy:
- *   https://nguyennam90.github.io/Check_thiet_bi/
+ *   https://ktnvbdtls-oss.github.io/RA-SOAT-THIET-BI-2026/
  */
 const GITHUB_CONFIG = {
-  owner: "nguyennam90",
-  repo: "Check_thiet_bi",
+  owner: "ktnvbdtls-oss",
+  repo: "RA-SOAT-THIET-BI-2026",
   branch: "main",
   dataFile: "data/inventory.json",
 };
